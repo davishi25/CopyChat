6b9dab68772c036854b62cfcb9d911de46eb89b0
davishi <126983431+davishi25@users.noreply.github.com>
2024-11-28 15:19:19 -0600

    layout or smth
    why do imports not work

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ java {
 
 application {
     // Define the main class for the application.
-    mainClass = "copychat.App"
+    mainClass = "copychat.CopyChat"
 }
 
 tasks.named<Test>("test") {
--- app/src/test/java/copychat/AppTest.java
+++ app/src/test/java/copychat/AppTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     @Test void appHasAGreeting() {
-        App classUnderTest = new App();
+        CopyChat classUnderTest = new CopyChat();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
 }
